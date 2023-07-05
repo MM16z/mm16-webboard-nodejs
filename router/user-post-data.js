261771845e8f3bb95e871192534c6511d6f4320b
Min16z <min@finema.co>
2023-07-06 00:00:51 +0700

    [fix] - userpost query

--- router/user-post-data.js
+++ router/user-post-data.js
@@ -9,43 +9,55 @@ router.get("/user_posts/:offset", async (req, res) => {
   // const currentQuery = Number(req.query.currentQuery);
   const currentUserId = req.query.currentUserId || null;
   try {
-    const allpostQuery = `
-    SELECT post_id, post_from, post_title, post_content, post_createdAt,
-    CASE WHEN postliked.user_id IS NOT NULL THEN TRUE ELSE FALSE END as isLiked,
-    (
-      SELECT COUNT(at_post_id)
-      FROM "mm16-webboard".postliked
-      WHERE at_post_id = post_id
-    ) as post_liked_count,
-    COALESCE(
-      json_agg(
-        json_build_object(
-          'comment_id', comments.comment_id,
-          'comment_from', comments.comment_from,
-          'comment_content', comments.comment_content,
-          'comment_date', comments.comment_createdAt
-        )
-      ),
-      '[]'::json
-    ) as comments
-    FROM "mm16-webboard".posts
-    LEFT JOIN "mm16-webboard".comments ON comments.at_post_id = post_id
-    LEFT JOIN "mm16-webboard".postliked ON postliked.at_post_id = posts.post_id AND postliked.user_id = $1
-    GROUP BY post_id , postliked.user_id
-    ORDER BY post_id ASC
-    LIMIT $2
-    offset $3`;
-    const postcountQuery = `SELECT COUNT(post_id) as all_post_count FROM "mm16-webboard".posts`;
-    const allPost = await db.any(allpostQuery, [currentUserId, 6, offset]);
-    const postCount = await db.any(postcountQuery);
-    if (allPost && postCount) {
-      res.json({
-        postsCount: postCount[0],
-        allPosts: allPost,
-      });
-    }
+    const query = `
+      SELECT
+        posts.post_id,
+        posts.post_from,
+        posts.post_title,
+        posts.post_content,
+        posts.post_createdAt,
+        (postliked.user_id IS NOT NULL) AS isLiked,
+        (
+          SELECT COUNT(at_post_id)
+          FROM "mm16-webboard".postliked
+          WHERE at_post_id = posts.post_id
+        ) AS post_liked_count,
+        COALESCE(
+          (
+            SELECT json_agg(json_build_object(
+              'comment_id', comments.comment_id,
+              'comment_from', comments.comment_from,
+              'comment_content', comments.comment_content,
+              'comment_date', comments.comment_createdAt
+            ))
+            FROM "mm16-webboard".comments
+            WHERE comments.at_post_id = posts.post_id
+          ),
+          '[]'
+        ) AS comments
+      FROM
+      "mm16-webboard".posts
+        LEFT JOIN postliked ON postliked.at_post_id = posts.post_id AND postliked.user_id = $1
+      GROUP BY
+        posts.post_id , 
+      ORDER BY
+        posts.post_id ASC
+      LIMIT
+        $2, 6`;
+
+    const values = [currentUserId, offset];
+    const allPosts = await db.any(query, values);
+
+    const countQuery = `SELECT COUNT(post_id) as all_post_count FROM "mm16-webboard".posts`;
+    const postCount = await db.one(countQuery);
+
+    res.json({
+      postsCount: postCount.all_post_count,
+      allPosts: allPosts,
+    });
   } catch (error) {
     console.log(error);
+    res.json({ status: "error", message: error });
   }
 });
 
