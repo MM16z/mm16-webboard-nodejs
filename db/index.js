53e065e824535a1db79f77f463dd3ff5e6864c74
MM16z <76926089+MM16z@users.noreply.github.com>
2024-07-21 00:17:59 +0700

    change env

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const pgp = require("pg-promise")();
 
 const db = pgp(
-  `postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.DBHOST}/${process.env.DATABASE}`
+  `postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.DBHOST}/${process.env.DATABASE}?sslmode=require`
 );
 
 // const db = mysql.createConnection({
